e706b568d9f625a686788653b3c81496cb86c976
Savidya Sathyani <savidya19@gmail.com>
2018-04-24 14:16:58 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+var config = {
+    entry: './src/main.js', // entry point
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js', // place where bundled app will be served
+    },
+    devServer: {
+        inline: true, // autorefresh
+        port: 8080 // development port server
+    },
+    module: {
+        rules: [
+        {
+            test: /\.jsx?$/, // search for js files 
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }, 
+        {
+            test: /\.css?$/, // search for js files 
+            loader: ['style-loader','css-loader']
+        }
+    ]
+    },
+    mode: 'none'
+}
+module.exports = config;
