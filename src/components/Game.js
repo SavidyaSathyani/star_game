b474bceadc67604ee691ff1ae5d956181889cd9f
SavidyaSathyani <savidya19@gmail.com>
2018-04-26 12:53:36 +0530

    Fixed issue

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,35 +5,34 @@ import Answer from '../components/Answer';
 import Numbers from '../components/Numbers';
 
 class Game extends React.Component {
-    render() {
-        this.state = {
-            selectedNumbers: []
-        };
-
+    constructor() {
+        super();
+        this.state = { selectedNumbers: [] };
         this.selectNumber = (clickedNumber) => {
             console.log(clickedNumber);
             this.setState(prevState => ({
                 selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
-                //selectedNumbers: [prevState.selectedNumbers,clickedNumber]
             }));
             console.log(this.state.selectedNumbers);
         };
+    }
 
-        return ( 
+    render() {
+        return (
             <div className="jumbotron">
-                <div style={{marginLeft:"10px"}}>
+                <div style={{ marginLeft: "10px" }}>
                     <h1 >Play Stars</h1>
                     <p>This is a simple game to improve your addition skills in mathemetics.</p>
                 </div>
-                <hr/>
+                <hr />
                 <div className="container-fluid">
                     <div className="row">
                         <Stars />
                         <Button />
-                        <Answer selectedNumbers={this.state.selectedNumbers}/>
+                        <Answer selectedNumbers={this.state.selectedNumbers} />
                     </div>
-                    <br/>
-                    <Numbers selectedNumbers={this.state.selectedNumbers} selectNumber={this.selectNumber}/>
+                    <br />
+                    <Numbers selectedNumbers={this.state.selectedNumbers} selectNumber={this.selectNumber} />
                 </div>
             </div>
         );
