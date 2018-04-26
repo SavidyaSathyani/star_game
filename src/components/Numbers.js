972590bd3513a796df87898551a55a2005211d80
SavidyaSathyani <savidya19@gmail.com>
2018-04-26 12:17:28 +0530

    Add numbers component

---
+++ src/components/Numbers.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import _ from 'lodash';
+
+const Numbers = (props) => {
+    const numberClass = (number) => {
+        if(props.selectedNumbers.indexOf(number) >= 0){
+            return 'selected';
+        }
+    }
+    return(
+        <div className="card text-center">
+            {Numbers.list.map((number, i) => 
+                <span key={i} className={numberClass(number)} onClick={() => props.selectNumber(number)}>
+                    {number}
+                </span>)}
+        </div>
+    );
+};
+
+Numbers.list = _.range(1,10);
+
+export default Numbers;
